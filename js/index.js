386cb7d11c2c924602e5bbfd92f3c6424c45539f
Daniel Andreu <daniel1997ar@gmail.com>
2023-09-28 17:19:08 +0200

    Add new card button functionality

--- js/index.js
+++ js/index.js
@@ -18,6 +18,25 @@ const cardNames = [
 let visibleCard;
 let hiddenCard;
 
+const visibleCardElement = document.querySelector(".visible-card");
+const hiddenCardElement = document.querySelector(".hidden-card");
+const biggerButtonElement = document.querySelector(".bigger-button");
+const smallerButtonElement = document.querySelector(".smaller-button");
+const newCardButtonElement = document.querySelector(".new-card-button");
+const homeButtonElement = document.querySelector(".home-button");
+const visibleCardUpPositionElement =
+  visibleCardElement.querySelector(".position-up");
+const visibleCardMiddlePositionElement =
+  visibleCardElement.querySelector(".position-middle");
+const visibleCardDownPositionElement =
+  visibleCardElement.querySelector(".position-down");
+const hiddenCardUpPositionElement =
+  hiddenCardElement.querySelector(".position-up");
+const hiddenCardMiddlePositionElement =
+  hiddenCardElement.querySelector(".position-middle");
+const hiddenCardDownPositionElement =
+  hiddenCardElement.querySelector(".position-down");
+
 const generateDeck = () => {
   const deck = [];
 
@@ -35,6 +54,12 @@ const selectCards = () => {
     generateDeck()[Math.floor(Math.random() * generateDeck().length)];
   hiddenCard =
     generateDeck()[Math.floor(Math.random() * generateDeck().length)];
+  visibleCardUpPositionElement.textContent = visibleCard.cardSuit;
+  visibleCardMiddlePositionElement.textContent = visibleCard.cardName;
+  visibleCardDownPositionElement.textContent = visibleCard.cardSuit;
+  hiddenCardUpPositionElement.textContent = hiddenCard.cardSuit;
+  hiddenCardMiddlePositionElement.textContent = hiddenCard.cardName;
+  hiddenCardDownPositionElement.textContent = hiddenCard.cardSuit;
 };
 
 const compareCards = () => {
@@ -46,3 +71,7 @@ const compareCards = () => {
     return "equal";
   }
 };
+
+newCardButtonElement.addEventListener("click", (event) => {
+  selectCards();
+});
